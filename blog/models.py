47c04959077c1b11ef8bdfd5f7fc818ed8f59243
kryspinm97 <maneskryspin69@gmail.com>
2023-03-14 10:21:03 +0000

    Created a comment database model class

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,13 @@ from cloudinary.models import CloudinaryField
 
 class Car(models.Model):
     """Site User's Cars"""
+
     make = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     slug = models.SlugField(max_length=200, unique=True)
-    site_user = models.ForeignKey(User, on_delete = models.CASCADE, related_name= "car_user")
+    site_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="car_user"
+    )
     created_date = models.DateTimeField(auto_now_add=True)
     year = models.IntegerField()
     specifications = models.TextField()
@@ -19,15 +22,37 @@ class Car(models.Model):
     likes = models.ManyToManyField(User, related_name="car_likes", blank=True)
 
     class Meta:
-        """ List posts in a descending order """
-        ordering = ['-posted_date']
+        """List posts in a descending order"""
+
+        ordering = ["-posted_date"]
 
     def __str__(self):
-        """ Generating a title """
+        """Generating a title"""
         return f"{self.site_user} : {self.model} ({self.year})"
 
     def like(self, user):
         self.likes.add(user)
 
     def get_absolute_url(self):
-        return reverse('gallery')
+        return reverse("gallery")
+
+
+class Comment(models.Model):
+    """Comments on Car Post"""
+
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="car_comments"
+    )
+    text = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name="comment_likes")
+
+    def __str__(self):
+        return f"{self.author.username} on {self.car}"
+
+    def like(self, user):
+        self.likes.add(user)
+
+    def unlike(self, user):
+        self.likes.remove(user)
