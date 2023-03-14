c1319a8b2ae499bd5e57bd2ea380319c1cbae5ec
kryspinm97 <maneskryspin69@gmail.com>
2023-03-14 10:45:52 +0000

    Added Admin model for Car and Comment

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Car, Comment
 
 
-class AdminPost(SummernoteModelAdmin):
+@admin.register(Car)
+class CarAdmin(SummernoteModelAdmin):
+    """ Car Admin model """
+
+    list_display = ('make', 'model', 'year')
+    summernote_fields = ('specifications', 'rundown')
 
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+    """ Comment Admin Model """
+
+    list_display = ('author', 'created_date')
+    summernote_fields = ('text',)
