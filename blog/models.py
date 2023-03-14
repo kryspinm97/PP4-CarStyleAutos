1ba1a55173eba211f1b3aacf467e63291da66497
kryspinm97 <maneskryspin69@gmail.com>
2023-03-14 10:11:52 +0000

    Creating database model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+class Car(models.Model):
+    """Site User's Cars"""
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=200, unique=True)
+    site_user = models.ForeignKey(User, on_delete = models.CASCADE, related_name= "car_user")
+    created_date = models.DateTimeField(auto_now_add=True)
+    year = models.IntegerField()
+    specifications = models.TextField()
+    rundown = models.TextField()
+    car_image = CloudinaryField("image")
+    likes = models.ManyToManyField(User, related_name="car_likes", blank=True)
+
+    class Meta:
+        """ List posts in a descending order """
+        ordering = ['-posted_date']
+
+    def __str__(self):
+        """ Generating a title """
+        return f"{self.site_user} : {self.model} ({self.year})"
+
+    def like(self, user):
+        self.likes.add(user)
+
+    def get_absolute_url(self):
+        return reverse('gallery')
