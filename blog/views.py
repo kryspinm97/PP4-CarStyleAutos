bb7062ce0512f204ac378da49369f52ab2c47186
kryspinm97 <maneskryspin69@gmail.com>
2023-03-16 17:43:54 +0000

    Imported django models in views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.views.generic import ListView, DetailView
+from .models import Car, Comment
 
 # Create your views here.
