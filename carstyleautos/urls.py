3d1a78b2c985b65debb7c0a3569c2a600882a2ae
kryspinm97 <maneskryspin69@gmail.com>
2023-03-16 22:20:30 +0000

    Setting up template files + static css/images

--- carstyleautos/urls.py
+++ carstyleautos/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from blog import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path('', views.home, name='home'),
 ]
