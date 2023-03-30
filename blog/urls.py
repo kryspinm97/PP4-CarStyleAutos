72549f093718ae76dd1ab0e62fddd6d8473ae3ee
kryspinm97 <maneskryspin69@gmail.com>
2023-03-30 18:59:20 +0000

    Added post functions for login and register view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('summernote/', include('django_summernote.urls')),
     path('car_gallery/', views.CarGallery.as_view(), name='cargallery'),
     path('account/login', views.LoginView.as_view(), name='login'),
     path('account/register', views.RegisterView.as_view(), name='register'),
--- blog/views.py
+++ blog/views.py
@@ -25,8 +25,34 @@ class LoginView(View):
     def get(self, request):
         return render(request, 'account/login.html')
 
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password!')
+            return redirect('login')
+
 
 class RegisterView(View):
 
     def get(self, request):
         return render(request, 'account/register.html')
+
+    def post(self, request):
+
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+            return redirect('login')
+        else:
+            messages.error(request, 'Invalid registration details')
+            return redirect('register')
