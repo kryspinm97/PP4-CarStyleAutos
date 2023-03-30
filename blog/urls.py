d74c90167939ff7a77cb0b0b29d5f1ad9a6c27bf
kryspinm97 <maneskryspin69@gmail.com>
2023-03-30 20:15:48 +0000

    Added basic ability to create user + login

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('car_gallery/', views.CarGallery.as_view(), name='cargallery'),
     path('account/login', views.LoginView.as_view(), name='login'),
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views import generic, View
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from .models import Car, Comment
+from .forms import RegistrationForm, CarCreationForm
 
 
 class Home(View):
@@ -26,8 +29,8 @@ class LoginView(View):
         return render(request, 'account/login.html')
 
     def post(self, request):
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
@@ -54,5 +57,23 @@ class RegisterView(View):
             messages.success(request, f'Account created for {username}')
             return redirect('login')
         else:
-            messages.error(request, 'Invalid registration details')
-            return redirect('register')
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f'{field}: {error}')
+        return redirect('register')
+
+@login_required
+def create_car(request):
+    if request.method == 'POST':
+        form = CarCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            car = form.save(commit=False)
+            car.site_user = request.user
+            car.save()
+            messages.success(request, 'Car Added Successfuly')
+            return redirect('car_detail', pk=car.pk)
+        else:
+            messages.error(request, 'Invalid car details')
+    else:
+        form = CarCreationForm()
+    return render(request, 'create_car.html', {'form': form})
