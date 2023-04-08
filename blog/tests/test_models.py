8ae93c1954c9db4ab0aadf94b905b1ff8afa5de4
kryspinm97 <maneskryspin69@gmail.com>
2023-04-08 12:27:23 +0000

    Added initial tests for CommentModel

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -30,3 +30,44 @@ class CarModelTestCase(TestCase):
 
     def test_car_str_method(self):
         self.assertEqual(str(self.car), 'testuser : Camry (2020)')
+
+
+class CommentModelTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+
+        self.car = Car.objects.create(
+            make='Toyota',
+            model='Camry',
+            slug='toyota-camry-2020',
+            site_user=self.user,
+            year=2020,
+            specifications='<p> specs </p>',
+            rundown='<p> rundown </p>',
+            car_image='car_image.jpg'
+        )
+
+        self.comment = Comment.objects.create(
+            car=self.car,
+            author=self.user,
+            text='This is a test comment.'
+        )
+
+    def test_comment_creation(self):
+        self.assertEqual(self.comment.author.username, 'testuser')
+        self.assertEqual(self.comment.car, self.car)
+        self.assertEqual(self.comment.text, 'This is a test comment.')
+
+    def test_comment_like_method(self):
+        self.comment.like(self.user)
+        self.assertTrue(self.user in self.comment.likes.all())
+
+    def test_comment_unlike_method(self):
+        self.comment.like(self.user)
+        self.comment.unlike(self.user)
+        self.assertFalse(self.user in self.comment.likes.all())
+
+    def test_comment_str_method(self):
+        self.assertEqual(str(self.comment), 'testuser on testuser : Camry (2020)')
